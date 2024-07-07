bb909defeff8f2a901019d9a6bf40ed607319f86
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-07-07 16:51:57 -0300

    Fixed updating user changes in firestore.

--- woofi/ViewModel/User/UserViewModel.swift
+++ woofi/ViewModel/User/UserViewModel.swift
@@ -13,30 +13,22 @@ class UserViewModel: NSObject {
     init(user: User) {
         self.user = user
     }
-    
+        
+        
     func updateUser(name: String?, bio: String?) {
         guard let name = name, let bio = bio else { return }
         
         user.username = name
         user.bio = bio
         
-        
-        func updateUser(name: String?, bio: String?) {
-            guard let name = name, let bio = bio else { return }
-            
-            user.username = name
-            user.bio = bio
-            
-            
-            FirestoreService.shared.updateUserData(userId: user.id, data: [
-                FirestoreKeys.Users.username: name,
-                FirestoreKeys.Users.bio: bio
-            ]) { error in
-                if let error = error {
-                    print("Error updating user: \(error.localizedDescription)")
-                } else {
-                    print("User updated successfully")
-                }
+        FirestoreService.shared.updateUserData(userId: user.id, data: [
+            FirestoreKeys.Users.username: name,
+            FirestoreKeys.Users.bio: bio
+        ]) { error in
+            if let error = error {
+                print("Error updating user: \(error.localizedDescription)")
+            } else {
+                print("User updated successfully")
             }
         }
     }
