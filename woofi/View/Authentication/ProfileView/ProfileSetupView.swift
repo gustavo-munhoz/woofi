472d11a5c47a2f6e92cd0624cafa58fa6079a047
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-07-10 23:14:08 -0300

    Added google auth icons and created profile setup view.

---
+++ woofi/View/Authentication/ProfileView/ProfileSetupView.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileSetupView.swift
+//  woofi
+//
+//  Created by Gustavo Munhoz Correa on 10/07/24.
+//
+
+import Foundation
---
+++ woofi/View/Authentication/ProfileView/ProfileSetupViewController.swift
@@ -0,0 +1,8 @@
+//
+//  ProfileSetupViewController.swift
+//  woofi
+//
+//  Created by Gustavo Munhoz Correa on 10/07/24.
+//
+
+import Foundation
