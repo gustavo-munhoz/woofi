16569c5a880399322c561b6d02362d151ba489b3
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-07-17 08:19:47 -0300

    Implemented edit pet view.

--- woofi/View/Home/Pets/PetSingle/Editing/EditPetView.swift
+++ woofi/View/Home/Pets/PetSingle/Editing/EditPetView.swift
@@ -5,4 +5,196 @@
 //  Created by Gustavo Munhoz Correa on 17/07/24.
 //
 
-import Foundation
+import UIKit
+import SnapKit
+
+class EditPetView: UIView {
+    
+    // MARK: - Properties
+    
+    weak var viewModel: PetViewModel?
+    
+    var onPictureButtonTapped: (() -> Void)?
+    
+    // MARK: - Subviews
+    
+    private(set) lazy var changePictureButton: UIButton = {
+        var config = UIButton.Configuration.bordered()
+        config.image = UIImage(systemName: "person.crop.circle")
+        config.preferredSymbolConfigurationForImage = .init(pointSize: 32)
+        config.imagePadding = 24
+        config.baseForegroundColor = .primary
+        
+        config.attributedTitle = AttributedString("Change Pet Picture", attributes: AttributeContainer([
+            .font: UIFont.preferredFont(forTextStyle: .title3)
+        ]))
+        
+        let view = UIButton(configuration: config)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.heightAnchor.constraint(equalToConstant: 90).isActive = true
+        view.addTarget(self, action: #selector(handlePictureButtonTap), for: .touchUpInside)
+        view.contentHorizontalAlignment = .leading
+        
+        return view
+    }()
+
+    
+    private(set) lazy var pictureStackView: EditStackView = {
+        let view = EditStackView(title: "Pet Picture", editView: changePictureButton)
+        
+        return view
+    }()
+    
+    private(set) lazy var petNameTextView: PaddedTextView = {
+        let view = PaddedTextView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemGray5
+        view.layer.cornerRadius = 8
+        view.font = .preferredFont(forTextStyle: .title3)
+        view.delegate = self
+        
+        return view
+    }()
+    
+    private(set) lazy var petNameStackView: EditStackView = {
+        EditStackView(title: "Pet name", editView: petNameTextView)
+    }()
+    
+    private(set) lazy var petBreedTextView: PaddedTextView = {
+        let view = PaddedTextView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemGray5
+        view.layer.cornerRadius = 8
+        view.font = .preferredFont(forTextStyle: .title3)
+        view.delegate = self
+        
+        return view
+    }()
+    
+    private(set) lazy var petBreedStackView: EditStackView = {
+        EditStackView(title: "Pet breed", editView: petBreedTextView)
+    }()
+    
+    private(set) lazy var petAgeTextView: PaddedTextView = {
+        let view = PaddedTextView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .systemGray5
+        view.layer.cornerRadius = 8
+        view.font = .preferredFont(forTextStyle: .title3)
+        view.delegate = self
+        
+        return view
+    }()
+    
+    private(set) lazy var petAgeStackView: EditStackView = {
+        EditStackView(title: "Pet Age", editView: petAgeTextView)
+    }()
+    
+    // MARK: - Class Methods
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .systemBackground
+        
+        addSubviews()
+        setupConstraints()
+        setupTapGestureRecognizer()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviews() {
+        addSubview(pictureStackView)
+        addSubview(petNameStackView)
+        addSubview(petBreedStackView)
+        addSubview(petAgeStackView)
+    }
+    
+    private func setupConstraints() {
+        pictureStackView.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).offset(24)
+            make.left.right.equalToSuperview().inset(24)
+            make.height.equalTo(150)
+        }
+        
+        changePictureButton.imageView?.snp.makeConstraints { make in
+            make.width.height.equalTo(65)
+            make.centerY.equalToSuperview()
+            make.left.equalToSuperview().inset(14)
+        }
+        
+        changePictureButton.titleLabel?.snp.makeConstraints { make in
+            make.centerY.top.bottom.equalToSuperview()
+            make.left.equalTo(changePictureButton.imageView?.snp.right ?? changePictureButton).offset(24)
+            make.right.equalToSuperview().inset(12)
+        }
+        
+        petNameStackView.snp.makeConstraints { make in
+            make.top.equalTo(pictureStackView.snp.bottom).offset(16)
+            make.left.right.equalTo(pictureStackView)
+            make.height.equalTo(90)
+        }
+        
+        petBreedStackView.snp.makeConstraints { make in
+            make.top.equalTo(petNameStackView.snp.bottom).offset(16)
+            make.left.right.equalTo(petNameStackView)
+            make.height.equalTo(90)
+        }
+        
+        petAgeStackView.snp.makeConstraints { make in
+            make.top.equalTo(petBreedStackView.snp.bottom).offset(16)
+            make.left.right.equalTo(petNameStackView)
+            make.height.equalTo(90)
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @objc func handlePictureButtonTap() {
+        onPictureButtonTapped?()
+    }
+    
+    @objc func handleTap() {
+        endEditing(true)
+    }
+    
+    func setupTapGestureRecognizer() {
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.addGestureRecognizer(tapGesture)
+    }
+    
+    func updatePetImage(_ image: UIImage) {
+        let resizedImage = image.withRenderingMode(.alwaysOriginal)
+        changePictureButton.setImage(resizedImage, for: .normal)
+        
+        if let imageView = changePictureButton.imageView {
+            imageView.contentMode = .scaleAspectFill
+            imageView.layer.cornerRadius = imageView.frame.width / 2
+            imageView.clipsToBounds = true
+        }
+    }
+}
+
+// MARK: - UITextFieldDelegate
+
+extension EditPetView: UITextViewDelegate {
+    func textViewDidEndEditing(_ textView: UITextView) {
+        viewModel?.updatePet(
+            name: petNameTextView.text,
+            breed: petBreedTextView.text,
+            age: petAgeTextView.text
+        )
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        guard text != "\n" else { return false }
+        
+        let characterLimit = 20
+        let currentText: NSString = textView.text as NSString
+        let updatedText = currentText.replacingCharacters(in: range, with: text)
+        
+        return updatedText.count <= characterLimit
+    }
+}
--- woofi/View/Home/Pets/PetSingle/Editing/EditPetViewController.swift
+++ woofi/View/Home/Pets/PetSingle/Editing/EditPetViewController.swift
@@ -5,4 +5,113 @@
 //  Created by Gustavo Munhoz Correa on 17/07/24.
 //
 
-import Foundation
+import UIKit
+import PhotosUI
+
+class EditPetViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    private var editPetView = EditPetView()
+    
+    var viewModel: PetViewModel? {
+        didSet {
+            fillUI()
+            
+        }
+    }
+    
+    // MARK: - Class Methods
+    
+    func setViewModel(_ vm: PetViewModel) {
+        viewModel = vm
+    }
+    
+    override func loadView() {
+        view = editPetView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        editPetView.onPictureButtonTapped = presentImagePicker
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationItem.title = "Edit your pet"
+        navigationItem.largeTitleDisplayMode = .always
+    }
+    
+    override func viewWillLayoutSubviews() {
+        DispatchQueue.main.async {
+            if let imageView = self.editPetView.changePictureButton.imageView {
+                imageView.contentMode = .scaleAspectFill
+                imageView.layer.cornerRadius = imageView.frame.width / 2
+                imageView.clipsToBounds = true
+            }
+        }
+    }
+    
+    private func fillUI() {
+        editPetView.changePictureButton.setImage(
+            viewModel?.pet.picture ?? UIImage(systemName: "person.circle"),
+            for: .normal
+        )
+        
+        editPetView.petNameTextView.text = viewModel?.pet.name
+        editPetView.petBreedTextView.text = viewModel?.pet.breed
+        editPetView.petAgeTextView.text = viewModel?.pet.age
+    }
+    
+    // MARK: - Actions
+    
+    private func presentImagePicker() {
+        PHPhotoLibrary.requestAuthorization { status in
+            DispatchQueue.main.async {
+                switch status {
+                case .authorized, .limited:
+                    var config = PHPickerConfiguration()
+                    config.filter = .images
+                    config.selectionLimit = 1
+                    
+                    let picker = PHPickerViewController(configuration: config)
+                    picker.delegate = self
+                    self.present(picker, animated: true, completion: nil)
+                    
+                case .denied, .restricted, .notDetermined:
+                    let alert = UIAlertController(
+                        title: "Acesso às Fotos Negado",
+                        message: "Por favor, permita o acesso às suas fotos nas configurações do dispositivo.",
+                        preferredStyle: .alert
+                    )
+                    alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                    
+                @unknown default:
+                    break
+                }
+            }
+        }
+    }
+}
+
+// MARK: - PHPickerViewControllerDelegate
+
+extension EditPetViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        guard let result = results.first else { return }
+        
+        result.itemProvider.loadObject(ofClass: UIImage.self) { [weak self] (object, error) in
+            DispatchQueue.main.async {
+                if let image = object as? UIImage {
+                    self?.editPetView.updatePetImage(image)
+                    self?.viewModel?.updatePetPicture(image)
+                }
+            }
+        }
+    }
+}
