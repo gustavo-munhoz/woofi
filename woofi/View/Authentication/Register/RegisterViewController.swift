e7ce3617b501335c87ccc687d576520917f29f0f
Gustavo Munhoz Corrêa <95633339+gustavo-munhoz@users.noreply.github.com>
2024-07-23 09:14:09 -0300

    Implemented view actions. All authentication logic is now working, will start dealing with error feedback for user.

--- woofi/View/Authentication/Register/RegisterViewController.swift
+++ woofi/View/Authentication/Register/RegisterViewController.swift
@@ -6,10 +6,15 @@
 //
 
 import UIKit
+import Combine
 
 class RegisterViewController: UIViewController {
     
+    private var cancellables = Set<AnyCancellable>()
+    
     private var registerView = RegisterView()
+    
+    var viewModel = RegisterViewModel()
     
     override func loadView() {
         view = registerView
@@ -18,11 +23,104 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupViewActions()
+        setupViewModel()
+        setupSubscriptions()
+    }
+    
+    private func setupViewModel() {
+        registerView.viewModel = viewModel
+        viewModel.onSignUpSuccess = handleAuthSuccess(id:)
+        viewModel.onSignUpFailure = handleAuthFailure(with:)
+    }
+    
+    private func setupSubscriptions() {
+        viewModel.shouldSkipSetupProfilePublisher
+            .receive(on: RunLoop.main)
+            .sink { [weak self] _ in
+                self?.dismiss(animated: true)
+                DispatchQueue.main.async {
+                    self?.navigationController?.pushViewController(HomeViewController(), animated: true)
+                }
+                
+            }
+            .store(in: &cancellables)
+    }
+    
+    // MARK: - Authentication logic
+    
+    enum AuthError: Int, Error {
+        case userAlreadyExists
+        case unknownError
+        
+        init(error: Error) {
+            let nsError = error as NSError
+            
+            switch nsError.code {
+            case AuthError.userAlreadyExists.rawValue:
+                self = .userAlreadyExists
+                
+            default:
+                self = .unknownError
+            }
+        }
+    }
+    
+    private func handleAuthSuccess(id: UserId) {
+        Task {
+            do {
+                let userExists = try await FirestoreService.shared.checkIfUserExists(id: id)
+                if userExists { throw AuthError.userAlreadyExists }
+                
+                DispatchQueue.main.async { [weak self] in
+                    let vc = ProfileSetupViewController()
+                    vc.setUserId(id)
+                    
+                    print("User signed up successfully.")
+                    self?.navigationController?.pushViewController(vc, animated: true)
+                }
+                
+            } catch {
+                let authError = AuthError(error: error)
+                switch authError {
+                case .userAlreadyExists:
+                    print("Error: user already exists. Will try to skip profile setup.")
+                    viewModel.handleUserAlreadyExists(id: id)
+                case .unknownError:
+                    print("Unknown error creating user: \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
+    private func handleAuthFailure(with error: Error) {
+        print("Authentication failed with error: \(error.localizedDescription)")
+    }
+    
+    // MARK: - Actions
+    
+    private func setupViewActions() {
+        registerView.startAnimation()
         registerView.onCloseButtonTap = handleCloseButtonTap
+        registerView.onSignUpButtonTap = handleSignUpTap
+        registerView.onGoogleButtonTap = handleSignUpWithGoogleTap
+        registerView.onAppleButtonTap = handleSignUpWithApple
     }
     
     private func handleCloseButtonTap() {
         dismiss(animated: true)
+    }
+    
+    private func handleSignUpTap() {
+        viewModel.signUpUser()
+    }
+    
+    private func handleSignUpWithGoogleTap() {
+        viewModel.signUpWithGoogle(viewControllerRef: self)
+    }
+    
+    private func handleSignUpWithApple() {
+        viewModel.signUpWithApple()
     }
 }
 
--- woofi/ViewModel/Authentication/RegisterViewModel.swift
+++ woofi/ViewModel/Authentication/RegisterViewModel.swift
@@ -7,10 +7,128 @@
 
 import Foundation
 import Combine
+import UIKit
 
 class RegisterViewModel {
+    
+    // MARK: - Attributes
+    
     @Published var isSigningUp = false
     
     @Published var email: String = ""
     @Published var password: String = ""
+    
+    var onSignUpSuccess: ((UserId) -> Void)?
+    var onSignUpFailure: ((Error) -> Void)?
+    
+    private var lastAuthType: AuthenticationType?
+    private(set) var shouldSkipSetupProfilePublisher = PassthroughSubject<Bool, Never>()
+    
+    // MARK: - Sign up logic
+    
+    func signUpUser() {
+        guard !email.isEmpty, !password.isEmpty else { return }
+                
+        Task {
+            isSigningUp = true
+            lastAuthType = .register
+            
+            do {
+                let authResult = try await AuthenticationService.shared.registerUser(
+                    withEmail: email,
+                    password: password,
+                    additionalData: [:]
+                )
+                onSignUpSuccess?(authResult.user.uid)
+                
+            } catch {
+                onSignUpFailure?(error)
+            }
+        }
+    }
+    
+    func signUpWithGoogle(viewControllerRef vc: UIViewController) {
+        lastAuthType = .googleLogin
+        
+        Task {
+            do {
+                let authResult = try await AuthenticationService.shared.loginUser(withGoogleForm: vc)
+                
+                print("User signed in with Google: \(authResult.user.uid)")
+                onSignUpSuccess?(authResult.user.uid)
+                
+            } catch {
+                print("Error signing in with google: \(error.localizedDescription)")
+                onSignUpFailure?(error)
+            }
+        }
+    }
+    
+    func signUpWithApple() {
+        lastAuthType = .appleSignIn
+        
+        AuthenticationService.shared.signInWithApple { result in
+            switch result {
+            case .success(let userId):
+                print("User signed in with Apple: \(userId)")
+                self.onSignUpSuccess?(userId)
+                
+            case .failure(let failure):
+                print("Error signing in with Apple: \(failure.localizedDescription)")
+                self.onSignUpFailure?(failure)
+                
+            }
+        }
+    }
+    
+    // MARK: - Handling user already exists
+    
+    func handleUserAlreadyExists(id: UserId) {
+        guard lastAuthType == .googleLogin || lastAuthType == .appleSignIn else { return }
+        
+        Task {
+            do {
+                guard let user = await fetchUserFromFirebase(id: id) else { return }
+                
+                Session.shared.currentUser = user                
+                shouldSkipSetupProfilePublisher.send(true)
+            }
+        }
+    }
+}
+
+extension RegisterViewModel {
+    func fetchUserFromFirebase(id: UserId) async -> User? {
+        do {
+            let userData = try await FirestoreService.shared.fetchUserData(userId: id)
+            
+            let username = userData[FirestoreKeys.Users.username] as? String ?? "User"
+            let bio = userData[FirestoreKeys.Users.bio] as? String
+            let groupId = userData[FirestoreKeys.Users.groupID] as? String ?? UUID().uuidString
+            
+            let user = User(
+                id: id,
+                username: username,
+                bio: bio,
+                groupID: groupId
+            )
+            
+            if let profilePictureUrl = userData[FirestoreKeys.Users.profileImageUrl] as? String,
+               let url = URL(string: profilePictureUrl) {
+                do {
+                    let image = try await FirestoreService.shared.fetchImage(from: url)
+                    user.profilePicture = image
+                    
+                } catch {
+                    print("Error fetching profile picture during authentication: \(error.localizedDescription)")
+                }
+            }
+            
+            return user
+            
+        } catch {
+            print("Error fetching or building user from Firebase: \(error.localizedDescription)")
+            return nil
+        }
+    }
 }
